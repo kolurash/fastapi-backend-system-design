962b9f2e7f66c32204b695f8cff4cafd91ee0f6a
Kolu <kolurashmitha@gmail.com>
2026-05-16 17:47:26 +0530

    Added dependency injection

--- app/api/users.py
+++ app/api/users.py
@@ -1,11 +1,28 @@
-from fastapi import APIRouter
+# from fastapi import APIRouter
+
+# router = APIRouter(prefix="/users", tags=["Users"])
+
+
+# @router.get("/")
+# async def get_users():
+#     return {
+#         "users": [
+#             {"id": 1, "name": "Rashmitha"},
+#             {"id": 2, "name": "Developer"}
+#         ]
+#     }
+
+
+from fastapi import APIRouter, Depends
+from app.dependencies.auth import verify_user
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.get("/")
-async def get_users():
+async def get_users(user=Depends(verify_user)):
     return {
+        "status": user,
         "users": [
             {"id": 1, "name": "Rashmitha"},
             {"id": 2, "name": "Developer"}
